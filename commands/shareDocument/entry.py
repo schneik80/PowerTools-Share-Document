57cdf0d7101d60499dd355f9bb1111678f3c3f79
Kevin Schneider <schneik80@gmail.com>
2025-01-18 16:59:44 -0800

    if sharing is disabled. get a team web perma-url instead

--- commands/shareDocument/entry.py
+++ commands/shareDocument/entry.py
@@ -117,8 +117,10 @@ def command_execute(args: adsk.core.CommandEventArgs):
         return
 
     if isShareAllowed is False:
+        permLink = app.activeDocument.designDataFile.fusionWebURL
+        clipboardText(permLink)
         ui.messageBox(
-            "Sharing is not allowed.\nPlease check if your Team Hub Administrator has disabled sharing",
+            f"Sharing is not allowed. Please check if your Team Hub Administrator has disabled sharing.<br><br>A private perma-link was copied to clipboard instead. This link will only allow Team hub members access to the document details page.",
             "Share Document",
             0,
             2,
