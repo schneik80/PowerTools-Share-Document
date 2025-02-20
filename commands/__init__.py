5d8aa033a3ee6668e23275c43c4ba9d75a488ef7
Kevin Schneider <schneik80@gmail.com>
2025-02-19 20:57:20 -0800

    Move open on web to branch, remove from main

--- commands/__init__.py
+++ commands/__init__.py
@@ -7,26 +7,20 @@ from .shareDocument import entry as shareDocument
 from .shareSettings import entry as shareSettings
 from .shareOpenDesktop import entry as shareOpenDesktop
 
-# from .shareOpenOnWeb import entry as shareOpenOnWeb
-
-# add your imported modules to this list.
 # Fusion will automatically call the start() and stop() functions.
 commands = [
     shareDocument,
     shareSettings,
     shareOpenDesktop,
-    # shareOpenOnWeb,
 ]
 
 
-# Assumes you defined a "start" function in each of your modules.
 # The start function will be run when the add-in is started.
 def start():
     for command in commands:
         command.start()
 
 
-# Assumes you defined a "stop" function in each of your modules.
 # The stop function will be run when the add-in is stopped.
 def stop():
     for command in commands:
